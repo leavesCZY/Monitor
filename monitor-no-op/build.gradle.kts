2de6fc6364c58a50312a52dfc7d396c9c7fefbf6
leavesCZY <1990724437@qq.com>
2024-09-28 21:19:18 +0800

    release:v1.8.2

--- monitor-no-op/build.gradle.kts
+++ monitor-no-op/build.gradle.kts
@@ -35,15 +35,15 @@ val signingKeyId = properties["signing.keyId"]?.toString()
 val signingPassword = properties["signing.password"]?.toString()
 val signingSecretKeyRingFile = properties["signing.secretKeyRingFile"]?.toString()
 val mavenCentralUserName = properties["mavenCentral.username"]?.toString()
-val mavenCentralPassword = properties["mavenCentral.password"]?.toString()
 val mavenCentralEmail = properties["mavenCentral.email"]?.toString()
+val mavenCentralPassword = properties["mavenCentral.password"]?.toString()
 
 if (signingKeyId != null
     && signingPassword != null
     && signingSecretKeyRingFile != null
     && mavenCentralUserName != null
-    && mavenCentralPassword != null
     && mavenCentralEmail != null
+    && mavenCentralPassword != null
 ) {
     publishing {
         publications {
